d3184076db9066e3b1a903a30f90364545010dbd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-16 22:10:34 +0000

    Deploy jzempel/jzempel.github.io to github.com/jzempel/jzempel.github.io.git:master

---
+++ index.js
@@ -0,0 +1,79 @@
+function getUrl(path) {
+  var index = location.href.lastIndexOf('/');
+  var url = location.href.substring(0, index);
+
+  return url + '/' + path;
+}
+
+function getXmlDocument(url) {
+  var retVal = null;
+
+  if (window.ActiveXObject) {
+    retVal = new ActiveXObject('Msxml2.FreeThreadedDOMDocument');
+    retVal.preserveWhiteSpace = true;
+    retVal.async = false;
+    retVal.load(url);
+  } else {
+    var xmlHttpRequest = new XMLHttpRequest();
+
+    xmlHttpRequest.open('get', url, false /*asynchronous*/);
+    xmlHttpRequest.overrideMimeType('text/xml');
+    xmlHttpRequest.send(null);
+
+    if (xmlHttpRequest.status == 200) {
+      retVal = xmlHttpRequest.responseXML;
+    }
+  }
+
+  return retVal;
+}
+
+function setGoogleAnalytics(trackingId) {
+  window.GoogleAnalyticsObject = 'ga';
+  window.ga = function() { window.ga.q.push(arguments); };
+  window.ga.q = [];
+  window.ga.l = (new Date()).getTime();
+  window.ga('create', trackingId, 'auto');
+  window.ga('send', 'pageview');
+}
+
+function transform(xmlUrl, xslUrl, links) {
+  var retVal = null;
+  var xmlDocument = getXmlDocument(xmlUrl);
+  var xslDocument = getXmlDocument(xslUrl);
+
+  if (xmlDocument != null && xslDocument != null) {
+    var myLinks = '';
+
+    if (links != null) {
+      for (var link in links) {
+        myLinks += links[link] + ',' + link + ';';
+      }
+    }
+
+    if (window.ActiveXObject) {
+      var xslTemplate = new ActiveXObject('MSXML2.XSLTemplate');
+
+      xslTemplate.stylesheet = xslDocument;
+
+      var xsltProcessor = xslTemplate.createProcessor();
+
+      xsltProcessor.addParameter('myLinks', myLinks);
+      xsltProcessor.input = xmlDocument;
+      xsltProcessor.transform();
+      retVal = xsltProcessor.output;
+    } else if (typeof XSLTProcessor != 'undefined') {
+      var xsltProcessor = new XSLTProcessor();
+
+      xsltProcessor.setParameter(null /*namespace*/, 'myLinks', myLinks);
+      xsltProcessor.importStylesheet(xslDocument);
+
+      var transformXml = xsltProcessor.transformToDocument(xmlDocument);
+      var xmlSerializer = new XMLSerializer();
+
+      retVal = xmlSerializer.serializeToString(transformXml);
+    }
+  }
+
+  return retVal;
+}
